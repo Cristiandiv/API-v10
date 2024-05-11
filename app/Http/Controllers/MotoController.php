fdddd1c52993209e15e2f8b94edcb18d5effe7ab
cristiandiv <devmil.ctrl@gmail.com>
2024-05-10 21:51:27 -0300

    api funcionando

--- app/Http/Controllers/MotoController.php
+++ app/Http/Controllers/MotoController.php
@@ -51,24 +51,64 @@ class MotoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(moto $moto)
+    public function show(string $id)
     {
-        //
+        $moto = Moto::find($id);
+
+        if($moto){
+            return 'Moto localizada.'.$moto.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
+        else{
+            return 'Moto não localizada.'.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, moto $moto)
+    public function update(Request $request, string $id)
     {
-        //
+        $dadosMotos = $request->all();
+        $validarDados = Validator::make($dadosMotos, [
+            'marca' => 'required',
+            'modelo' => 'required',
+            'cor' => 'required',
+            'ano' => 'required',
+        ]);
+
+        if($validarDados->fails()){
+            return 'Dados Invalidos.'.$validarDados->error(true). 500;
+        }
+
+        $moto = Moto::find($id);
+        $moto->marca = $dadosMotos['marca'];
+        $moto->modelo = $dadosMotos['modelo'];
+        $moto->cor = $dadosMotos['cor'];
+        $moto->ano = $dadosMotos['ano'];
+
+        $retorno = $moto->save();
+
+        if($retorno){
+            return 'Dados atualizados com Sucesso.'.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
+        else{
+            return 'Dados não atualizados.'.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
+    
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(moto $moto)
+    public function destroy(string $id)
     {
-        //
+        $dadosMotos = moto::find($id);
+        
+        if($dadosMotos->delete()){
+            return 'A moto foi deletada com sucesso.';
+        }
+
+        return 'A moto não foi deletada.'.response()->json([],Response::HTTP_NO_CONTENT);
     }
 }
